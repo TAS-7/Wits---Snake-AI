b60613196ca4fd53e76f1ec304c9f2eea0461219
TAS <44087952+TAS-7@users.noreply.github.com>
2020-09-08 13:14:46 +0200

    Update Point.java

--- Point.java
+++ Point.java
@@ -3,6 +3,7 @@ public class Point {
 	int x;
 	int y;
 	double distance = 0;
+	
 	int danger=0;
 	
 	
@@ -37,19 +38,44 @@ public class Point {
 		int south = this.y+1;
 		int east = this.x-1;
 		int west = this.x+1;
+		
 		if(north>=0 &&north<=49 && this.x>=0 &&this.x<=49) {
 			number = number + playField [north][this.x];
 		}
+		if(this.y-2>=0 &&this.y-2 <=49 && this.x>=0 &&this.x<=49 ) {
+			number = number + playField [this.y-2][this.x];
+		}
+		
 		
 		if(south>=0 &&south<=49 && this.x>=0 &&this.x<=49) {
 			number = number + playField [south][this.x];
 		}
+		if(this.y+2>=0 && this.y+2 <=49 && this.x>=0 &&this.x<=49) {
+			number = number + playField [this.y+2][this.x];
+		}
+		
+		
 		if(this.y>=0 &&this.y<=49 && west>=0 &&west<=49) {
 			number = number + playField [this.y][west];
 		}
+		if(this.y>=0 &&this.y<=49 && this.x+2>=0 &&this.x+2<=49) {
+			number = number + playField [this.y][this.x+2];
+		}
+		
+		
 		if(this.y>=0 &&this.y<=49 && east>=0 &&east<=49) {
 			number = number + playField [this.y][east];
 		}
+
+		if(this.y>=0 &&this.y<=49 && this.x-2>=0 &&this.x-2<=49) {
+			number = number + playField [this.y][this.x-2];
+		}
+		
+		
+		
+		
+		
+		
 		this.danger = number;
 		
 	}
